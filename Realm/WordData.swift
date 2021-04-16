1d667e8a4e1454765db84f937ffaea9dc7dc0245
jaeyung kim <jaeyungkim81@gaeasoft.co.kr>
2021-04-16 16:41:30 +0900

    add pod for realm

---
+++ Realm/WordData.swift
@@ -0,0 +1,21 @@
+//
+//  WordData.swift
+//  StudyEnglish
+//
+//  Created by jaeyung kim on 2021/04/16.
+//
+
+import Foundation
+
+//class WordData: Object {
+//
+//    @objc dynamic var engWord = ""
+//    @objc dynamic var section = ""
+//    @objc dynamic var meaning = ""
+//    @objc dynamic var partOfSpeech = ""
+//    @objc dynamic var example = ""
+//
+//    override static func primaryKey() -> String? {
+//        return "id"
+//    }
+//}
